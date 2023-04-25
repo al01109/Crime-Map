6111590c92af3ac87325bc0beb369b0588258f4c
Andrew <andrew.daniel.landon@gmail.com>
2023-04-25 17:05:19 +0100

    Added rendering tests

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,33 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
-import App from '../App';
+import {render, fireEvent} from '@testing-library/react-native';
+import QuickLink from '../src/components/QuickLink';
+import CrimeComment from '../src/components/Comment';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('QuickLink component', () => {
+  const mockLink = {
+    name: 'Test Link',
+    description: 'This is a test link',
+    image: 'https://test.com/image.jpg',
+    url: 'https://test.com',
+  };
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('renders the link name and description', () => {
+    const {getByText} = render(<QuickLink link={mockLink} />);
+    const name = getByText('Test Link');
+    const description = getByText('This is a test link');
+    expect(name).toBeTruthy();
+    expect(description).toBeTruthy();
+  });
+});
+
+const mockComment = {
+  id: 1,
+  description: 'This is a test comment',
+};
+
+describe('CrimeComment component', () => {
+  it('renders the comment description', () => {
+    const {getByText} = render(<CrimeComment comment={mockComment} />);
+    expect(getByText(mockComment.description)).toBeTruthy();
+  });
 });
